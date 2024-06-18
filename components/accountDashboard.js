eb3562689b674367a48b91b3bb4f1c3618baedee
razorus49 <107486515+razorus49@users.noreply.github.com>
2024-06-18 21:29:08 +0800

    blank app

---
+++ components/accountDashboard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    SectionList,
+    StatusBar,
+  } from 'react-native';
+
+  
+class accountDashboard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            profilePic: "/assets.images.react-logo.png",
+            name:'nerd',
+            totalMastery: 2000,
+            practiceCompleted: 100,
+            consecLogins: 10
+        }
+    }
+
+    render(){
+
+    }
+
+}
+
